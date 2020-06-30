f101da816173dc28cb848cca9afcc747d97aa9ab
Jacob Xi <xichao2014@gmail.com>
2020-06-30 17:32:22 +0800

    Create buildtools.groovy

---
+++ src/org/devops/buildtools.groovy
@@ -0,0 +1,11 @@
+package org.devops
+
+// build tools
+def Build(buildType, buildShell){
+    def buildTools = ["mvn":"M2", "ant":"ANT","gradle":"GRADLE","npm":"NPM"]
+
+    println("The current build tool is ${buildType}")
+    buildHome = tool buildTools[buildType]
+
+    sh "${buildHome}/bin/${buildType} ${buildShell}"
+}
