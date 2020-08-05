b43897e750013098dabc4150f909e7e8bbe1060b
i515190 <jacob.xi@sap,com>
2020-08-05 14:54:04 +0800

    update

--- src/org/devops/sonarqubeadv.groovy
+++ src/org/devops/sonarqubeadv.groovy
@@ -26,8 +26,8 @@ def SonarScan(projectName,projectDesc,projectPath){
         """
     }
     
-    //def qg = waitForQualityGate()
-    //if (qg.status != 'OK') {
-        //error "Pipeline aborted due to quality gate failure: ${qg.status}"
-    //}
+    def qg = waitForQualityGate()
+    if (qg.status != 'OK') {
+        error "Pipeline aborted due to quality gate failure: ${qg.status}"
+    }
 }
